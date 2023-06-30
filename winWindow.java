ecab93e4ad73f4ae7f552f04aa2625359bea30e1
Nico <nico.meissner2006@gmail.com>
2023-06-30 18:05:32 +0200

    added the code for the winWindow.java showing the user the message congratulation the users

---
+++ winWindow.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class winWindow implements ActionListener {
+    private JFrame frame;
+    private JLabel message;
+    private JButton close;
+
+    public winWindow(String player){
+        frame = new JFrame();
+        message = new JLabel();
+        close = new JButton();
+
+        setMessage(player);
+        setClose();
+
+        frame.add(message);
+        frame.add(close);
+
+        adjustJFrameSetting();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        //frame.setVisible(false);
+    }
+
+    /*
+    method set all the needed parameters for the JButton
+     */
+    private void setClose(){
+        close.setText("close");
+        close.setBounds(40, 50, 100, 32);
+        close.addActionListener(this);
+    }
+
+    /*
+    method set all the needed parameters to the JLabel
+     */
+    private void setMessage(String player){
+        message.setText("Congratulations! Player " + player + "has won this round.");
+        message.setBounds(10, 10, 300, 32);
+    }
+
+    /*
+    method sets all the needed parameters to the JFrame
+     */
+    private void adjustJFrameSetting() {
+        frame.setTitle("congratulations");
+        frame.setSize(400, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setResizable(true);
+        frame.setVisible(true);
+    }
+}
