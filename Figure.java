f619f6f560cad4c54d867588329bde57bed4a2b9
guemax <github-mg@guenther.cloud>
2023-07-26 21:04:10 +0200

    Remove unused attributes of Figure class

--- Figure.java
+++ Figure.java
@@ -8,18 +8,13 @@ enum State {
 public class Figure {
     public int field;
     public State state;
-    public boolean inBase;
-    public boolean inHouse;
     public int color;
-    public boolean finished;
     public boolean placeOption;
 
     Figure(int fieldNew, int colorNew){
-        inBase = true;
-        inHouse = false;
+	state = State.IN_BASE;
         field = fieldNew;
         color = colorNew;
-        finished = false;
         placeOption = false;
     }
 }
