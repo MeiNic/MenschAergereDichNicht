b87c0da7fe287130d9d6338ee53cfb46b1b01761
guemax <github-mg@guenther.cloud>
2023-07-31 18:18:24 +0200

    Add `LoadedDice` implementation of `Dice`

---
+++ LoadedDice.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+class LoadedDice implements Dice {
+    private static Random rand = new Random();
+
+    public int roll() {
+	int[] sampleSpace = {1, 1, 2, 2, 3, 3, 4, 4, 5, 6};
+	return sampleSpace[rand.nextInt(sampleSpace.length)];
+    }
+}
