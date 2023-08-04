37754dcf9a5cda10d66197384e23ac3205f61478
guemax <github-mg@guenther.cloud>
2023-08-04 07:58:14 +0200

    Add interface `Logger`

---
+++ Logger.java
@@ -0,0 +1,7 @@
+interface Logger {
+    public static void debug(String message);
+    public static void info(String message);
+    public static void warn(String message);
+    public static void error(String message);
+    public static void fatal(String message);
+}
