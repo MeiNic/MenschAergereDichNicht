3eb5f5a8566d952e17fbb99b9c0a95c2d9971b25
guemax <github-mg@guenther.cloud>
2023-08-14 17:17:16 +0200

    Adjust run.sh to switch to `src` folder before running `java Main`

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 # A small utility for running the game while developing. This script
 # ensures that the program is recompiled completely by removing any
 # .class files before starting javac.
+cd src/
+
 printf "Cleaning up... "
 rm *.class
 echo "done."
@@ -12,3 +14,5 @@ echo "done."
 
 echo "Starting game."
 java Main
+
+cd ../
