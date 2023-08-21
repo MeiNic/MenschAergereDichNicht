46579f591ad3f5f57e7168276d144aab88cc258e
Nico <nico.meissner2006@gmail.com>
2023-08-21 12:17:22 +0200

    added request if rules are accepted

--- src/Landingpage.java
+++ src/Landingpage.java
@@ -148,17 +148,22 @@ public class Landingpage extends JFrame implements ActionListener, ChangeListene
     }
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == startGame){
-            setVisible(false);
-            String[] names = getNames();
-            // TODO: Remove this `-1` by passing the "real" number of
-            // human players to `BackEnd` and switching a `<=` to a `<` in
-            // a loop of its constructor. This has to be done right after
-            // the pull request for refactoring this file has been fully
-            // merged with master. @guemax on 2023/08/16.
-            int numberOfPlayers = getNumberOfHumanPlayers() - 1;
-            boolean fillWithBots = getBotsSelection();
+            if (getUnderstoodStatus()){
+                setVisible(false);
+                String[] names = getNames();
+                // TODO: Remove this `-1` by passing the "real" number of
+                // human players to `BackEnd` and switching a `<=` to a `<` in
+                // a loop of its constructor. This has to be done right after
+                // the pull request for refactoring this file has been fully
+                // merged with master. @guemax on 2023/08/16.
+                int numberOfPlayers = getNumberOfHumanPlayers() - 1;
+                boolean fillWithBots = getBotsSelection();
 
-            new GameBoardGui(names, numberOfPlayers, fillWithBots);
+                new GameBoardGui(names, numberOfPlayers, fillWithBots);
+            }else {
+                add(notChecked);
+            }
+
         }else if (e.getSource() == rulesButton){
             //open a new Rules-Window
         }
