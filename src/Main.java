3dba695bc31712c2819b6f66678828bbbe395515
Nico <nico.meissner2006@gmail.com>
2023-09-23 17:45:48 +0200

    replaced tabs with spaces

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,24 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-	Logger logger = LoggerFactory.getLoggerInstance();
-	logger.info("Starting game.");
+        Logger logger = LoggerFactory.getLoggerInstance();
+        logger.info("Starting game.");
 
-	Thread quittingHook = new Thread(() -> logger.info("Quitting game."));
-	Runtime.getRuntime().addShutdownHook(quittingHook);
-	
+        Thread quittingHook = new Thread(() -> logger.info("Quitting game."));
+        Runtime.getRuntime().addShutdownHook(quittingHook);
+
         try {
-	    logger.info("Try setting look and feel of windows.");
+            logger.info("Try setting look and feel of windows.");
             UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-   
-	    logger.info("Instantiating landingpage.");
-	    new Landingpage();
+
+            logger.info("Instantiating landingpage.");
+            new Landingpage();
         } catch (UnsupportedLookAndFeelException e) {
-	    logger.fatal("Unable to set the look and feel of windows.");
+            logger.fatal("Unable to set the look and feel of windows.");
             e.printStackTrace();
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-	    logger.fatal("An unexpected error occured.");
-	    e.printStackTrace();
-	}
+            logger.fatal("An unexpected error occured.");
+            e.printStackTrace();
+        }
     }
 }
