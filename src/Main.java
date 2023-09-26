994f25dc2c57cba96595c39385fb81ea12000932
Nico <nico.meissner2006@gmail.com>
2023-09-26 16:54:27 +0200

    manipulations in Main

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,14 @@ public class Main {
 
         Thread quittingHook = new Thread(() -> logger.info("Quitting game."));
         Runtime.getRuntime().addShutdownHook(quittingHook);
+        String[] names = {"yellow", "green", "blue", "red"};
 
         try {
             logger.info("Try setting look and feel of windows.");
             UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 
-            logger.info("Instantiating landingpage.");
-            new Landingpage();
+            logger.info("Instantiating gameboard.");
+            new GameBoardGui(names, 4, false);
         } catch (UnsupportedLookAndFeelException e) {
             logger.fatal("Unable to set the look and feel of windows.");
             e.printStackTrace();
