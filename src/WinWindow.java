024df91838364f9a2bb891236d89c5f11d6db992
Nico <nico.meissner2006@gmail.com>
2023-12-21 22:28:02 +0100

    updated the winWindow to the new design

--- src/WinWindow.java
+++ src/WinWindow.java
@@ -1,36 +1,99 @@
 package src;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 
-public class WinWindow implements ActionListener {
+public class WinWindow{
     private JFrame frame;
     private JLabel message;
-    private JButton close;
-    private JButton nextGame;
-    private Logger logger;
+    private ImageTextPanel close;
+    private ImageTextPanel nextGame;
+    private final Font jetBrainsMonoSemiBold;
+    private static final Color defaultForegroundColor = Color.decode("#f3f5f9");
+    private static final Color defaultBackgroundColor = Color.decode("#6c6f85");
+    Logger logger;
 
     public WinWindow(String player){
         frame = new JFrame();
         message = new JLabel();
-        close = new JButton();
-        nextGame = new JButton();
+        close = new ImageTextPanel("button-idle", "close");
+        nextGame = new ImageTextPanel("button-idle", "new game");
         logger = LoggerFactory.getLoggerInstance();
 
-        //set component values
-        message.setText("Congratulations! Player " + player + " has won this round.");
+        //configure font
+        try {
+            jetBrainsMonoSemiBold = Font.createFont(Font.TRUETYPE_FONT,
+                    new File("fonts/jetBrainsMono/JetBrainsMono-SemiBold.ttf")).deriveFont(13f);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(jetBrainsMonoSemiBold);
+        } catch (FontFormatException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //configure the message
+        message.setText("<html><body>Congratulations! Player " + player + " <br> has won this round.</body></html>");
         message.setBounds(10, 10, 295, 32);
+        message.setFont(jetBrainsMonoSemiBold);
+        message.setForeground(defaultForegroundColor);
+        message.setBackground(defaultBackgroundColor);
 
-        close.setText("close");
+        //configure close button
         close.setBounds(140, 50, 100, 32);
-        close.addActionListener(this);
+        close.setFont(jetBrainsMonoSemiBold);
+        close.setForeground(defaultForegroundColor);
+        close.setBackground(defaultBackgroundColor);
+        close.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                logger.info("Closing game.");
+                frame.setVisible(false);
+            }
 
-        nextGame.setText("new game");
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                close.setImage("button-hovered");
+                frame.repaint();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                close.setImage("button-idle");
+                frame.repaint();
+            }
+        });
+
+        //configure nextGame button
         nextGame.setBounds(20, 50, 100, 32);
-        nextGame.addActionListener(this);
+        nextGame.setFont(jetBrainsMonoSemiBold);
+        nextGame.setForeground(defaultForegroundColor);
+        nextGame.setBackground(defaultBackgroundColor);
+        nextGame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                logger.info("Closing WinWindow.");
+                frame.setVisible(false);
+                logger.info("Starting new game.");
+                new Main();
+            }
 
-        //add elements
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                nextGame.setImage("button-hovered");
+                frame.repaint();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                nextGame.setImage("button-idle");
+                frame.repaint();
+            }
+        });
+
+        //add elements to frame
         frame.add(message);
         frame.add(close);
         frame.add(nextGame);
@@ -40,20 +103,9 @@ public class WinWindow implements ActionListener {
         frame.setSize(315, 150);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
+        frame.getContentPane().setBackground(defaultBackgroundColor);
         frame.setResizable(true);
         frame.setVisible(true);
         logger.info("Displaying WinWindow.");
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == nextGame){
-            logger.info("Closing WinWindow.");
-            frame.setVisible(false);
-            logger.info("Starting new game.");
-            new Main();
-        } else if (e.getSource() == close) {
-            logger.info("Closing game.");
-            frame.setVisible(false);
-        }
     }
 }
