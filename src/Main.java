84eb5f3faa50129208e52208c40c54381e3a1138
guemax <code-mg@mailbox.org>
2023-12-26 09:13:43 +0100

    Set theme font globally
    Solution inspired by
    https://stackoverflow.com/questions/7434845/setting-the-default-font-of-swing-program

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,39 @@ package src;
 import javax.swing.*;
 
 public class Main {
+    private static final Logger logger = LoggerFactory.getLoggerInstance();
+    private static final Theme theme = new Theme();
+    
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLoggerInstance();
         logger.info("Starting game.");
 
         Thread quittingHook = new Thread(() -> logger.info("Quitting game."));
         Runtime.getRuntime().addShutdownHook(quittingHook);
 
         try {
-            logger.info("Try setting look and feel of windows.");
-            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-
-            logger.info("Instantiating Landingpage.");
-            new Landingpage();
-        } catch (UnsupportedLookAndFeelException e) {
-            logger.fatal("Unable to set the look and feel of windows.");
-            e.printStackTrace();
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-            logger.fatal("An unexpected error occured.");
-            e.printStackTrace();
+	    logger.info("Try setting the look and feel of windows.");
+	    setLookAndFeel();
+	} catch (ClassNotFoundException | InstantiationException
+		 | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            logger.fatal("Unable to set the look and feel of windows:" + e);
+	    System.exit(1);
         }
+
+	logger.info("Instantiating Landingpage.");
+	new Landingpage();
+    }
+
+    private static void setLookAndFeel() throws ClassNotFoundException,
+						InstantiationException,
+						IllegalAccessException,
+						UnsupportedLookAndFeelException {
+	UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+
+	UIManager.put("Button.font", theme.font);
+	UIManager.put("CheckBox.font", theme.font);
+	UIManager.put("Label.font", theme.font);
+	UIManager.put("CheckBoxMenuItem.font", theme.font);
+	UIManager.put("Panel.font", theme.font);
+	UIManager.put("TextField.font", theme.font);
     }
 }
