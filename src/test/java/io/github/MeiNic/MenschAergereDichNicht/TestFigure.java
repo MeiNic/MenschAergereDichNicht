f04f47a59a3f68ec69f48898edeb17f378dbfca2
Max Günther <code-mg@mailbox.org>
2025-02-19 17:42:18 +0100

    Add more tests for demonstrative purposes

--- src/test/java/io/github/MeiNic/MenschAergereDichNicht/TestFigure.java
+++ src/test/java/io/github/MeiNic/MenschAergereDichNicht/TestFigure.java
@@ -1,11 +1,43 @@
 package io.github.MeiNic.MenschAergereDichNicht;
 
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 
 class TestFigure {
-    @Test
-    void moveByValue() {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 37, 38, 39})
+    void moveByValueDoesNotWrapField(int value) {
 	Figure figure = new Figure(0, 0, "");
+	figure.moveByValue(value);
+	assertEquals(value, figure.getField());
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {40, 41, 42})
+    void moveByValueDoesWrapField(int value) {
+	Figure figure = new Figure(0, 0, "");
+	figure.moveByValue(value);
+	assertEquals(value - 40, figure.getField());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 37, 38, 39, 40, 41, 42})
+    void moveByValueDoesNotWrapProgress(int value) {
+	Figure figure = new Figure(0, 0, "");
+	figure.moveByValue(value);
+	assertEquals(value, figure.getProgress());
+    }
+
+    // TODO: Maybe support this in the future? - @guemax, 2025-02-19
+    // @ParameterizedTest
+    // @ValueSource(ints = {-1, -2, -3})
+    // void moveByValueDoesNotAcceptNegativeIntegers(int value) {
+    // 	Figure figure = new Figure(0, 0, "");
+    // 	assertThrows(AssertionError.class, () -> {
+    // 		figure.moveByValue(value);
+    // 	    });
+    // }
 }
