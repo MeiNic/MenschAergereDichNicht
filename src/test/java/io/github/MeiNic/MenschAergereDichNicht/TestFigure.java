058259909775b031cd46baa2cfbea0034b927384
Max Günther <code-mg@mailbox.org>
2025-02-19 17:11:56 +0100

    Add simple test for Figure

---
+++ src/test/java/io/github/MeiNic/MenschAergereDichNicht/TestFigure.java
@@ -0,0 +1,11 @@
+package io.github.MeiNic.MenschAergereDichNicht;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestFigure {
+    @Test
+    void moveByValue() {
+	Figure figure = new Figure(0, 0, "");
+    }
+}
