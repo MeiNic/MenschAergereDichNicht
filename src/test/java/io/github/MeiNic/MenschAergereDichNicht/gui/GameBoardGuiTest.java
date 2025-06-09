02f3ddf62fb40acdb3c9da8fd7906fcbf60fac1e
MeiNic <nico.meissner2006@gmail.com>
2025-06-09 15:52:25 +0200

    example test

--- src/test/java/io/github/MeiNic/MenschAergereDichNicht/gui/GameBoardGuiTest.java
+++ src/test/java/io/github/MeiNic/MenschAergereDichNicht/gui/GameBoardGuiTest.java
@@ -1,4 +1,27 @@
 package io.github.MeiNic.MenschAergereDichNicht.gui;
 
+import io.github.MeiNic.MenschAergereDichNicht.BackEnd;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GameBoardGuiTest {
+
+    GameBoardGui gameBoardGui;
+
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("java.awt.headless", "false");
+
+
+        BackEnd backEnd = new BackEnd(new String[]{"Test", "Test", "Test", "Test"}, 4, false);
+        gameBoardGui = new GameBoardGui(backEnd);
+    }
+
+    @Test
+    public void test(){
+        assertEquals(1, 1);
+    }
 }
