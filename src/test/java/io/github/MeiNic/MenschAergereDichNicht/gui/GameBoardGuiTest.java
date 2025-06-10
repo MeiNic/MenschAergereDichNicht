9a872e70e6f1bd2dae98ef1b0cde0b5d86775771
MeiNic <nico.meissner2006@gmail.com>
2025-06-10 20:12:38 +0200

    removed unused import

--- src/test/java/io/github/MeiNic/MenschAergereDichNicht/gui/GameBoardGuiTest.java
+++ src/test/java/io/github/MeiNic/MenschAergereDichNicht/gui/GameBoardGuiTest.java
@@ -1,6 +1,5 @@
 package io.github.MeiNic.MenschAergereDichNicht.gui;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
