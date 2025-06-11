1dbebf84d103b0cc177903caf226c89bf49d949a
Max Günther <code-mg@mailbox.org>
2025-06-11 13:38:10 +0200

    Update copyright years

--- src/main/java/io/github/MeiNic/MenschAergereDichNicht/figure/Figure.java
+++ src/main/java/io/github/MeiNic/MenschAergereDichNicht/figure/Figure.java
@@ -1,5 +1,5 @@
 // This file is part of MenschAergereDichNicht.
-// Copyright (C) 2024-2025 MeiNic, TastingComb and contributors.
+// Copyright (C) 2023-2025 MeiNic, TastingComb and contributors.
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
