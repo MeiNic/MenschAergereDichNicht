c634815c9613183eb1a380cb2dfb295ef0792c63
Max Günther <code-mg@mailbox.org>
2025-08-14 15:05:54 +0200

    Add license header to tests

--- src/test/java/io/github/MeiNic/MenschAergereDichNicht/gui/GameBoardGuiTest.java
+++ src/test/java/io/github/MeiNic/MenschAergereDichNicht/gui/GameBoardGuiTest.java
@@ -1,3 +1,19 @@
+// This file is part of MenschAergereDichNicht.
+// Copyright (C) 2023-2025 MeiNic, TastingComb and contributors.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package io.github.MeiNic.MenschAergereDichNicht.gui;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -27,18 +43,17 @@ public class GameBoardGuiTest {
         System.setProperty("java.awt.headless", "true");
 
         // testdata
-        String[] names = {"Player1", "Player2", "Player3", "Player4"};
+        String[] names = { "Player1", "Player2", "Player3", "Player4" };
         backEnd = new BackEnd(names, 4, false);
 
         // using Mockito to mock JFrame construction
-        mockedJFrame =
-                Mockito.mockConstruction(
-                        JFrame.class,
-                        (mock, context) -> {
-                            // Mock configuration for JFrame;
-                            Container mockContainer = mock(Container.class);
-                            when(mock.getContentPane()).thenReturn(mockContainer);
-                        });
+        mockedJFrame = Mockito.mockConstruction(
+                JFrame.class,
+                (mock, context) -> {
+                    // Mock configuration for JFrame;
+                    Container mockContainer = mock(Container.class);
+                    when(mock.getContentPane()).thenReturn(mockContainer);
+                });
 
         // Erstelle GameBoardGui - ohne HeadlessException dank Mock
         gameBoardGui = new GameBoardGui(backEnd);
